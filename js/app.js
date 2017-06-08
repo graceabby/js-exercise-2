426babafbbb727775e0d6193b9c426e4a7e9b788
ACE User <aceuser@e140-s09.lab.lagcc>
2017-06-07 21:11:18 -0400

    created to do script

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var userSubmit = document.querySelector('#user-submit');
+
+function addToDo(event){
+	event.preventDefault();
+
+	var userInput = document.querySelector('#user-input');
+	var toDoList = document.querySelector('#to-do-list');
+		
+	if (userInput.value === ''){
+		return false; // will stop prevent the execution of code following
+
+
+	}
+
+		toDoList.innerHTML += '<li>' + userInput.value + '</li>';
+		userInput.value = '';
+}
+
+userSubmit.addEventListener('click',addToDo, false);
